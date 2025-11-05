31c53accbef0ae08974d8cf784426fee2feeff1e
Chiara <34109996+chiaramooney@users.noreply.github.com>
2025-11-05 11:41:39 -0800

    Update to 2.0.0-experimental

--- test-app/preload.js
+++ test-app/preload.js
@@ -6,16 +6,11 @@ const {
 
 contextBridge.exposeInMainWorld("windowsAI", {
   generateText: async (prompt) => {
-    try{
-        var readyState = LanguageModel.GetReadyState();
-        if (readyState == AIFeatureReadyState.NotReady) {
-            await LanguageModel.EnsureReadyAsync();
-        } else if (readyState == AIFeatureReadyState.Ready) {
-            var languageModel = await LanguageModel.CreateAsync();
-            if (languageModel) {
-                var result = await languageModel.GenerateResponseAsync(prompt);
-                return result.Text;
-            }
+    try {
+        var languageModel = await LanguageModel.CreateAsync();
+        if (languageModel) {
+            var result = await languageModel.GenerateResponseAsync(prompt);
+            return result.Text;
         }
     } catch (error) {
         console.error("Error generating text:", error);
